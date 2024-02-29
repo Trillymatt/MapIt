ae4dd9b4872aa1af3df566773174c007d23d56b8
zachkjones512 <zachkjones512@gmail.com>
2024-02-28 23:23:45 -0600

    Added marker support for a few buildings on campus: GAB, PHYS, BLB, NTDP

--- Main Page/index.js
+++ Main Page/index.js
@@ -5,4 +5,19 @@ document.addEventListener("DOMContentLoaded", function() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
+    const buildings = new Map();
+    buildings.set("GAB", [33.21316482090388, -97.1481464082617]);
+    buildings.set ("NTDP", [33.253667671664694, -97.15253791766376]);
+    buildings.set("BLB", [33.208923343929065, -97.14768127787393]);
+    buildings.set("PHYS", [33.213146118569824, -97.14658036227581]);
+
+    var input = document.getElementsByClassName("input-class")[0]; 
+    input.addEventListener("input", function() {
+        if (buildings.has(input.value)) {
+            var coordinates = buildings.get(input.value);
+            L.marker(coordinates).addTo(map);
+            console.log(`Added marker successfully at ${input.value}`)
+        }
+    });
 });
